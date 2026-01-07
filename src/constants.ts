8eda580a6aa5660895ab36b3e1a8b61fe57e2d1c
antim0118 <my.antim@inbox.ru>
2026-01-07 13:19:07 +0500

    Set types for lpyt version constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
+import type { LPYTVersion } from "./types/LPYTVersion";
 import type { TabType } from "./types/TabType";
 
-export const VERSIONS = ["0.5_beta_mcsm", "0.5_beta_2", "0.5_beta_3"];
+export const VERSIONS: LPYTVersion[] = ["0.5_beta_mcsm", "0.5_beta_2", "0.5_beta_3"];
 
-export const DEFAULT_VERSION = "0.5_beta_3";
+export const DEFAULT_VERSION: LPYTVersion = "0.5_beta_3";
 
 export const HEADER_TABS: TabType[] = [
   {
--- src/types/LPYTVersion.ts
+++ src/types/LPYTVersion.ts
@@ -1,1 +1,1 @@
-export type LPYTVersion = "0.31" | "0.4" | "0.5_beta_mcsc" | "0.5_beta_2" | "0.5" | "0.6";
+export type LPYTVersion = "0.31" | "0.4" | "0.5_beta_mcsm" | "0.5_beta_2" | "0.5_beta_3" | "0.5" | "0.6";
