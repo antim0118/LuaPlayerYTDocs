31e3d58513d3d1dca1061368dff762a5116fc9da
antim0118 <my.antim@inbox.ru>
2026-01-07 14:57:43 +0500

    Closing sidebar on select

--- src/components/layout/Sidebar/SidebarItem.tsx
+++ src/components/layout/Sidebar/SidebarItem.tsx
@@ -3,6 +3,8 @@ import { Box, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useColorPalette } from "../../ui/ColorMode";
 import { LuExternalLink } from "react-icons/lu";
+import { useContext } from "react";
+import { AppContext } from "@/AppContext";
 
 type Props = {
   item: SidebarItemType;
@@ -11,15 +13,21 @@ type Props = {
 
 export const SidebarItem = ({ item, isSelected }: Props) => {
   const navigate = useNavigate();
+  const { setMobileMenuIsOpened } = useContext(AppContext);
   const { accentColor, selectedFontColor } = useColorPalette();
   const isExternalLink = item.link.startsWith("http");
+
+  const onClickHandler = () => {
+    if (!isExternalLink) navigate(`../${item.link}`, { relative: "path" });
+    setMobileMenuIsOpened?.(false);
+  };
 
   return (
     <Box key={item.link}>
       <Link
         href={isExternalLink ? item.link : "#"}
         target={isExternalLink ? "_blank" : undefined}
-        onClick={() => !isExternalLink && navigate(`../${item.link}`, { relative: "path" })}
+        onClick={onClickHandler}
         fontWeight={isSelected ? "bolder" : "normal"}
         bgColor={isSelected ? accentColor : undefined}
         color={isSelected ? selectedFontColor : undefined}
