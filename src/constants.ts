23298af1e527e3ab38d083b56ede6148aab100f5
antim0118 <my.antim@inbox.ru>
2026-01-10 14:15:08 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 import type { LPYTVersion } from "./types/LPYTVersion";
 import type { TabType } from "./types/TabType";
 
+export const HEADER_HEIGHT = 100;
+
 export const VERSIONS: LPYTVersion[] = ["0.5_beta_mcsm", "0.5_beta_2", "0.5_beta_3"];
 
 export const DEFAULT_VERSION: LPYTVersion = "0.5_beta_3";
