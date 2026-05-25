1c55a86b5edb5332755c6347dbb582b10008eca9
antim0118 <my.antim@inbox.ru>
2026-05-26 00:09:50 +0500

    Moved logo component

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -1,4 +1,4 @@
-import { useColorPalette } from './ColorMode'
+import { useColorPalette } from '../../components/ui/ColorMode'
 
 type Props = {
   width?: number
@@ -6,7 +6,7 @@ type Props = {
 }
 
 export const Logo = ({ width = 88, height = 118 }: Props) => {
-  const { accentColorHex } = useColorPalette()
+  const { accentColor } = useColorPalette()
 
   return (
     <svg width={width} height={height} viewBox="0 0 88 118" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -14,7 +14,7 @@ export const Logo = ({ width = 88, height = 118 }: Props) => {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M0 86L41.5 0L48 9.5L15.5 76H27.5L54 23L87.5 86H34L18 117.5L9 113L22.5 86H0ZM55 44.5L39 76H71L55 44.5Z"
-        fill={accentColorHex}
+        fill={accentColor}
       />
     </svg>
   )
