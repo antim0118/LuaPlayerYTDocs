bc3731ab68193cc568d6dfe88604746d9ba1c9e6
antim0118 <my.antim@inbox.ru>
2026-05-26 23:19:12 +0500

    Installed MDX

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import mdx from '@mdx-js/rollup'
+import { resolve } from 'node:path'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    {
+      enforce: 'pre', ...mdx({
+        providerImportSource: '@mdx-js/react'
+      }) },
+    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
+    tsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+  },
 })
