b2e9f95dd915d80449794152cc2a49a1d20a79e6
antim0118 <my.antim@inbox.ru>
2026-05-26 20:47:15 +0500

    Added border to ImageSnippet

--- src/components/ImageSnippet.tsx
+++ src/components/ImageSnippet.tsx
@@ -1,16 +1,30 @@
 import { Flex, Image, Text } from '@chakra-ui/react'
 import { type PropsWithChildren } from 'react'
+import { useColorPalette } from './ui/ColorMode'
 
 type Props = PropsWithChildren & {
   src: string
-  maxW?: string
+  maxWidth?: string
   margin?: number
 }
 
-export const ImageSnippet = ({ src, maxW, children, margin }: Props) => {
+export const ImageSnippet = ({ src, maxWidth, children, margin }: Props) => {
+  const { borderColor } = useColorPalette()
+
   return (
-    <Flex flexDirection="column" alignItems="center" m={margin}>
-      <Image src={src} maxW={maxW ?? 'max-content'} rounded="xl" />
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      margin={margin}
+    >
+      <Image
+        src={src}
+        maxWidth={maxWidth ?? 'max-content'}
+        rounded="xl"
+        boxSizing="border-box"
+        border="1px solid"
+        borderColor={borderColor}
+      />
       <Text fontStyle="italic" fontSize="small" mt={2}>
         {children}
       </Text>
