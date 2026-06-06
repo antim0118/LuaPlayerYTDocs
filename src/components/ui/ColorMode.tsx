64682d6fbf03dcf17391729518f5e3b7c6cd0158
antim0118 <my.antim@inbox.ru>
2026-06-06 23:46:41 +0500

    Update ColorMode.tsx

--- src/components/ui/ColorMode.tsx
+++ src/components/ui/ColorMode.tsx
@@ -1,8 +1,5 @@
-import type { SpanProps } from '@chakra-ui/react'
-import { Span } from '@chakra-ui/react'
 import { ThemeProvider, useTheme } from 'next-themes'
 import type { ThemeProviderProps } from 'next-themes'
-import * as React from 'react'
 
 export type ColorModeProviderProps = ThemeProviderProps
 
@@ -12,7 +9,7 @@ export const ColorModeProvider = (props: ColorModeProviderProps) => {
 
 export type ColorMode = 'light' | 'dark'
 
-export interface UseColorModeReturn {
+interface UseColorModeReturn {
   colorMode: ColorMode
   setColorMode: (colorMode: ColorMode) => void
   toggleColorMode: () => void
@@ -35,34 +32,6 @@ export const useColorModeValue = <T,>(light: T, dark: T) => {
   const { colorMode } = useColorMode()
   return colorMode === 'dark' ? dark : light
 }
-
-export const LightMode = React.forwardRef<HTMLSpanElement, SpanProps>(function LightMode(props, ref) {
-  return (
-    <Span
-      color="fg"
-      display="contents"
-      className="chakra-theme light"
-      colorPalette="gray"
-      colorScheme="light"
-      ref={ref}
-      {...props}
-    />
-  )
-})
-
-export const DarkMode = React.forwardRef<HTMLSpanElement, SpanProps>(function DarkMode(props, ref) {
-  return (
-    <Span
-      color="fg"
-      display="contents"
-      className="chakra-theme dark"
-      colorPalette="gray"
-      colorScheme="dark"
-      ref={ref}
-      {...props}
-    />
-  )
-})
 
 export const useColorPalette = () => {
   const accentColor = useColorModeValue('#18181b', '#dc2626') // используется в лого
